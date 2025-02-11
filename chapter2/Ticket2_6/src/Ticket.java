cef0c64baa16eef3cfe794652df8359226d14680
zhuzedan <1031155817@qq.com>
2025-02-11 19:47:52 +0800

    修改

--- chapter2/Ticket2_6/src/Ticket.java
+++ chapter2/Ticket2_6/src/Ticket.java
@@ -15,35 +15,35 @@ public class Ticket {
         System.out.println("请输入购买票数：");
         ticketNum = input.nextInt();
 
-if (time) {
-    if (option.equals("A")) {
-        ticketType = "成人票";
-        totalPrice = ticketNum * 30;
-    } else if (option.equals("S")) {
-        ticketType = "学生票";
-        totalPrice = ticketNum * 15;
-    } else if (option.equals("O")) {
-        ticketType = "老人票";
-        totalPrice = ticketNum * 30 * 0.6;
-    } else if (option.equals("C")) {
-        ticketType = "儿童票";
-        totalPrice = ticketNum * 10;
-    }
-} else {
-    if (option.equals("A")) {
-        ticketType = "成人票";
-        totalPrice = ticketNum * 45;
-    } else if (option.equals("S")) {
-        ticketType = "学生票";
-        totalPrice = ticketNum * 45 * 0.5;
-    } else if (option.equals("O")) {
-        ticketType = "老人票";
-        totalPrice = ticketNum * 45 * 0.6;
-    } else if (option.equals("C")) {
-        ticketType = "儿童票";
-        totalPrice = ticketNum * 15;
-    }
-}
+        if (time) {
+            if (option.equals("A")) {
+                ticketType = "成人票";
+                totalPrice = ticketNum * 30;
+            } else if (option.equals("S")) {
+                ticketType = "学生票";
+                totalPrice = ticketNum * 15;
+            } else if (option.equals("O")) {
+                ticketType = "老人票";
+                totalPrice = ticketNum * 30 * 0.6;
+            } else if (option.equals("C")) {
+                ticketType = "儿童票";
+                totalPrice = ticketNum * 10;
+            }
+        } else {
+            if (option.equals("A")) {
+                ticketType = "成人票";
+                totalPrice = ticketNum * 45;
+            } else if (option.equals("S")) {
+                ticketType = "学生票";
+                totalPrice = ticketNum * 45 * 0.5;
+            } else if (option.equals("O")) {
+                ticketType = "老人票";
+                totalPrice = ticketNum * 45 * 0.6;
+            } else if (option.equals("C")) {
+                ticketType = "儿童票";
+                totalPrice = ticketNum * 15;
+            }
+        }
 
         System.out.println("您购买了" + ticketNum + "张" + ticketType +
                 ",请支付购票金额：" + "\n" + totalPrice + " 元");
